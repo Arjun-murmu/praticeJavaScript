94ffd3020c23520a744e2194b787079cc1e733d3
Arjun Murmu <92778275+Arjun-murmu@users.noreply.github.com>
2024-05-10 13:13:59 +0530

    Create gradecheck.js
    Student Marksheet using JavaScript

---
+++ gradecheck.js
@@ -0,0 +1,35 @@
+let mark = prompt("Enter your mark: ");
+/*
+if(mark>=90){
+    console.log("A Grade.");
+}
+else if(mark>=70){
+    console.log("B Grade.")
+}
+else if(mark>=60){
+    console.log("C Grade.")
+}
+else if(mark>=50){
+    console.log("D Grade.")
+}
+else{
+    console.log("F Grade.")
+}
+*/
+// OR 
+    console.log("you are  : ")
+if(mark >= 90 && mark <= 100){
+    console.log("Grade A.");
+}
+else if(mark >= 70 && mark <= 89){
+    console.log("B Grade.")
+}
+else if(mark >= 60 && mark <= 69){
+    console.log("C Grade.")
+}
+else if(mark >= 50 && mark <= 59){
+    console.log("D Grade.")
+}
+else if(mark >= 0 && mark <= 49){
+    console.log("F Grade.")
+}
